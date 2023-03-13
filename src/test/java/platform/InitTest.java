f296713f788b5558abd2ad1107557b7c0b446db1
Mohammed Gesreha <mohammed.gesreha@caresyntax.com>
2023-03-13 16:14:52 +0000

    Update InitTest.java

--- src/test/java/platform/InitTest.java
+++ src/test/java/platform/InitTest.java
@@ -18,7 +18,7 @@ public class InitTest {
     protected static void launchBrowser(){
         CustomListeners.setAcceptedRate(40);
         playwright = Playwright.create();
-        browser= playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        browser= playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
     }
 
     @BeforeMethod
