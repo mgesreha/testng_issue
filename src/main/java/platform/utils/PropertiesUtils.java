144f16b2371e9fa1de4536a5a5ae5a6907fd872d
mgesreha <mohammed.gesreha@caresyntax.com>
2023-03-17 12:39:06 +0100

    Add PropertiesUtils and parametrise headless mode

--- src/main/java/platform/utils/PropertiesUtils.java
+++ src/main/java/platform/utils/PropertiesUtils.java
@@ -7,17 +7,14 @@ import java.util.Properties;
 
 public class PropertiesUtils {
 
-     public static Properties getProperties(String path) {
-        File propertyFile = new File("target/classes/" +path);
+     public static Properties getProperties(String fileName) {
+        File propertyFile = new File("target/classes/" + fileName);
         Properties props = new Properties();
-         System.out.println(propertyFile.getAbsolutePath());
          try {
-//            inputStream = PropertiesUtils.class.getClassLoader().getResourceAsStream(propertyFile.getPath());
-            // load the properties file
             props.load(new FileInputStream(propertyFile));
         } catch (IOException ex) {
             throw new RuntimeException("[AUT-ERROR] Properties were not loaded from file with name :: "
-                    + path + "instead throw exception:", ex);}
+                    + fileName + "instead throw exception:", ex);}
         return props;
     }
 }
